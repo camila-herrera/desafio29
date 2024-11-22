26bb7eb2e33176510211c79ed6b44a11c591b8f2
camila-herrera <camila.h.gonzalez@gmail.com>
2024-11-22 20:30:04 -0300

    ahora si

--- operaciones.js
+++ operaciones.js
@@ -1,20 +1,21 @@
 const fs = require('fs');
 
-
 const registrar = (nombre, edad, animal, color, enfermedad) => {
-    const cita = { nombre, edad, animal, color, enfermedad };
-    const data = JSON.stringify(cita); //
+    const cita = { nombre, edad, animal, color, enfermedad }; 
+    const data = JSON.stringify(cita);
     fs.appendFileSync('citas.json', data + '\n'); 
     console.log(`Cita de ${nombre} registrada exitosamente.`);
 };
 
 const leer = () => {
     const data = fs.readFileSync('citas.json', 'utf-8');
-    const citas = data.trim().split('\n').filter(line => line);
-    JSON.parse(`[${citas.join(',')}]`).forEach(cita => {console.log(cita); });};
+    const citas = data.trim().split('\n').filter(line => line); 
+    citas.forEach(cita => console.log(JSON.parse(cita))); 
+};
 
 const vaciar = () => {
-    fs.writeFileSync('citas.json', "[]\n"); 
-    console.log("Todas las citas han sido eliminadas.");};
+    fs.writeFileSync('citas.json', "");
+    console.log("Todas las citas han sido eliminadas.");
+};
 
 module.exports = { registrar, leer, vaciar };
